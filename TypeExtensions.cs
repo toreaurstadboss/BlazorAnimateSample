eeffaf6c59c7b0191de67cd880d23f4e3c4f530b
Tore Aurstad <tore.aurstad@gmail.com>
2023-05-18 15:33:04 +0200

    Finished v1

---
+++ TypeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace BlazorAnimationSample
+{
+    public static class TypeExtensions
+    {
+
+
+        /// <summary>
+        ///     A generic extension method that aids in reflecting 
+        ///     and retrieving any attribute that is applied to an `Enum`.
+        /// </summary>
+        public static TAttribute? GetAttribute<TAttribute>(this Enum enumValue)
+                where TAttribute : Attribute
+        {
+            return enumValue?.GetType()
+                            ?.GetMember(enumValue.ToString())
+                            ?.FirstOrDefault()
+                            ?.GetCustomAttribute<TAttribute>();
+        }
+
+        public static string? GetDisplayName(this Enum enumValue)
+        {
+            return GetAttribute<DisplayAttribute>(enumValue)?.Name;
+        }
+
+    }
+}
