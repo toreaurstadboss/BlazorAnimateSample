9a0a421b218c09e562ca96a6e25ee9127aeaa9ff
Tore Aurstad <tore.aurstad@gmail.com>
2023-05-18 13:26:46 +0200

    Js in own file for the animation handling

---
+++ wwwroot/js/animate.js
@@ -0,0 +1,27 @@
+﻿
+function RegisterAnimationStartupTrigger(wrapperAnimationElementId, triggerElementId, triggerEventId) {
+    //debugger
+    var elem = document.getElementById(wrapperAnimationElementId);
+    elem.classList.remove('aos-init'); //remove aos-animate class to avoid auto loading animation on scroll
+    elem.classList.remove('aos-animate'); //remove aos-animate class to avoid auto loading animation on scroll
+    var triggerElement = document.getElementById(triggerElementId);
+    triggerElement.addEventListener(triggerEventId, function () { AddAosAnimateCssClass(elem, triggerEventId); }); //remove aos-animate class to avoid auto loading animation on scroll
+}
+
+function AddAosAnimateCssClass(el, triggerEventId) {
+    debugger
+
+    el.classList.remove('aos-init');
+    el.classList.remove('aos-animate');
+
+    if (triggerEventId.toLowerCase() == 'change') {
+        if (!event.target.checked) {
+            return; //in case this is a checkbox, only trigger on checked state
+        }
+    }
+
+    setTimeout(function () {
+        el.classList.add('aos-init');
+        el.classList.add('aos-animate');
+    }, 500);
+}
