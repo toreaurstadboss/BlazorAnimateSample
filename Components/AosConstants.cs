e5710a49850dc4a912fb0a708daf4613519cb8dd
Tore Aurstad <tore.aurstad@gmail.com>
2023-05-18 13:08:54 +0200

    Init commit

---
+++ Components/AosConstants.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazorAnimationSample.Components
+{
+    public static class AosConstants
+    {
+        public static class Animations
+        {
+
+            public const string fade = "fade";
+            public const string fade_up = "fade-up";
+            public const string fade_down = "fade-down";
+            public const string fade_left = "fade-left";
+            public const string fade_right = "fade-right";
+            public const string fade_up_right = "fade-up-right";
+            public const string fade_up_left = "fade-up-left";
+            public const string fade_down_right = "fade-down-right";
+            public const string fade_down_left = "fade-down-left";
+            public const string flip_up = "flip-up";
+            public const string flip_down = "flip-down";
+            public const string flip_left = "flip-left";
+            public const string flip_right = "flip-right";
+            public const string slide_up = "slide-up";
+            public const string slide_down = "slide-down";
+            public const string slide_left = "slide-left";
+            public const string slide_right = "slide-right";
+            public const string zoom_in = "zoom-in";
+            public const string zoom_in_up = "zoom-in-up";
+            public const string zoom_in_down = "zoom-in-down";
+            public const string zoom_in_left = "zoom-in-left";
+            public const string zoom_in_right = "zoom-in-right";
+            public const string zoom_out = "zoom-out";
+
+        }
+    }
+}
